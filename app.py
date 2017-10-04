784a9ae1fe273c84b5a5b1d6faa74ce602518457
rodion <rodiongork@github.com>
2017-10-04 16:59:29 +0300

    Rendering pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import os
 
 app = flask.Flask(__name__, static_folder = 's', template_folder = 't')
 
@@ -7,5 +8,19 @@ def index():
     return flask.render_template('index.html')
 
 @app.route('/q')
-def show_task():
-    return flask.render_template('task.html')
+@app.route('/q/')
+def show_task_root():
+    return show_task('')
+
+@app.route('/q/<path:page>')
+def show_task(page):
+    page = os.path.abspath('./pages') + '/' + page
+    if os.path.isdir(page):
+        page += '/_idx'
+    page += '.txt'
+    if not os.path.isfile(page):
+        content = "<h3>Not found: %s</h3>" % page
+    else:
+        with open(page) as f:
+            content = f.read()
+    return flask.render_template('task.html', body = content)
